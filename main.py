2c9f3cd97e086a69e3efa7ee02bc8cb5e45fdabe
Alex-Dumitriu <dumitriualexandruionut@gmail.com>
2026-01-06 23:15:40 +0200

    Model Training

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import mongo, scraper
 if __name__ == "__main__":
-    items = scraper.scrape_pages(pages=10)
+    items = scraper.scrape_pages(pages=130)
     print(f"Scraped {len(items)} items")
 
     stats = mongo.save_items_to_mongo(items)
